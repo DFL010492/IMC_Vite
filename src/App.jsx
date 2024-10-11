805184b0e4e33c876fa5424c0b7f2dcea730ca3c
Daniel F. Lima <daniel.f.lima@iesb.edu.br>
2024-10-11 10:47:21 -0300

    Tentativa de Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import IMC from './components/IMC'
+import Login from './components/Login';
+import Layout from './components/Layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   
   return (
+    
     <>
-     <IMC/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="Login" element={<Login />}
+          <Route path="IMC" element={<IMC />}
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/components/IMC.jsx
+++ src/components/IMC.jsx
@@ -1,46 +1,81 @@
 import React, { useState } from 'react';
+import './Todos.css'
 
 function IMC() {
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
   const [imc, setIMC] = useState(null);
+  const [classificacao, setClassificacao] = useState('');
 
   const calcularIMC = () => {
     const pesoNumerico = parseFloat(peso);
     const alturaNumerica = parseFloat(altura) / 100;
 
     if (pesoNumerico > 0 && alturaNumerica > 0) {
-      const imcCalculado = pesoNumerico / (alturaNumerica **2 );
+      const imcCalculado = pesoNumerico / (alturaNumerica ** 2);
       setIMC(imcCalculado.toFixed(2));
+      classificarIMC(imcCalculado);
     }
+  };
+
+  const classificarIMC = (imcValor) => {
+    let classificacaoResultado = '';
+
+    switch (true) {
+      case imcValor < 18.5:
+        classificacaoResultado = 'Abaixo do peso';
+        break;
+      case imcValor >= 18.5 && imcValor < 24.9:
+        classificacaoResultado = 'Peso normal';
+        break;
+      case imcValor >= 25 && imcValor < 29.9:
+        classificacaoResultado = 'Sobrepeso';
+        break;
+      case imcValor >= 30 && imcValor < 34.9:
+        classificacaoResultado = 'Obesidade Grau I';
+        break;
+      case imcValor >= 35 && imcValor < 39.9:
+        classificacaoResultado = 'Obesidade Grau II';
+        break;
+      default:
+        classificacaoResultado = 'Obesidade Grau III (mórbida)';
+        break;
+    }
+
+    setClassificacao(classificacaoResultado);
   };
 
   return (
     <>
       <div>
-        <h1>Calculadora de IMC</h1>
+        <h1><b>Calculadora de IMC</b></h1>
       </div>
       <div>
-        <p>Coloque o peso.</p>
+        <p><b>Coloque o peso em Kg.</b></p>
         <input 
           type="number" 
           value={peso} 
           onChange={(e) => setPeso(e.target.value)} 
-          placeholder="Insira o peso"
+          placeholder="Insira o peso em Kg."
         />
       </div>
       <div>
-        <p>Coloque a Altura em cm.</p>
+        <p><b>Coloque a Altura em cm.</b></p>
         <input 
           type="number" 
           value={altura} 
           onChange={(e) => setAltura(e.target.value)} 
-          placeholder="Insira a altura"
+          placeholder="Insira a altura em cm"
         />
       </div>
       <div>
         <button onClick={calcularIMC}>Calcular IMC</button>
-        {imc && <h2>Seu IMC é: {imc}</h2>}
+        {imc && (
+          <div>
+            <h2>Seu IMC é: {imc}</h2>
+            <h3>Classificação: {classificacao}</h3>
+          </div>
+        )}
       </div>
     </>
   );
